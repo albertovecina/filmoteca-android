79ddb9db0b3fff908f6713bd3e8717c39bf6ec8e
Alberto Vecina Sánchez <albertovecina@gmail.com>
2016-07-31 10:56:28 +0200

    *About: Added version code *Version code 28 *Version name 1.2.4

--- app/src/main/java/com/vsa/filmoteca/view/activity/AboutActivity.java
+++ app/src/main/java/com/vsa/filmoteca/view/activity/AboutActivity.java
@@ -1,9 +1,12 @@
 package com.vsa.filmoteca.view.activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.vsa.filmoteca.R;
 
@@ -13,20 +16,29 @@ import butterknife.ButterKnife;
 public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.layout_about_us_container)
-    LinearLayout mLayoutAboutUsContainer;
+    LinearLayout layoutAboutUsContainer;
     @BindView(R.id.layout_about_us_dialog)
-    LinearLayout mLayoutAboutUsDialog;
+    LinearLayout layoutAboutUsDialog;
+    @BindView(R.id.txt_about_us_build)
+    TextView textViewBuild;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
         ButterKnife.bind(this);
-        mLayoutAboutUsContainer.setOnClickListener(this);
+        layoutAboutUsContainer.setOnClickListener(this);
+        try {
+            PackageManager packageManager = this.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(this.getPackageName(), PackageManager.GET_META_DATA);
+            textViewBuild.setText(String.format(getString(R.string.version_code), packageInfo.versionCode));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onClick(View v) {
-        if (v != mLayoutAboutUsDialog)
+        if (v != layoutAboutUsDialog)
             finish();
     }
 }
