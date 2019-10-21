6cfcaf9e763fbcba14626beaf6fb10eb0741d8bf
Alberto Vecina Sánchez <albertovecina@gmail.com>
2019-10-21 09:49:57 +0200

    *Movie detail: Fixed movie detail parsing *About activity: Fixed version code method compativility *Progress dialog: Prevent non attached window crash

--- app/src/main/java/com/vsa/filmoteca/view/activity/AboutActivity.kt
+++ app/src/main/java/com/vsa/filmoteca/view/activity/AboutActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.pm.PackageInfoCompat
 import com.vsa.filmoteca.R
 import kotlinx.android.synthetic.main.activity_about_us.*
 
@@ -16,7 +17,7 @@ class AboutActivity : AppCompatActivity(), View.OnClickListener {
         try {
             val packageManager = this.packageManager
             val packageInfo = packageManager.getPackageInfo(this.packageName, PackageManager.GET_META_DATA)
-            textViewAboutUsBuild.text = String.format(getString(R.string.version_code), packageInfo.longVersionCode)
+            textViewAboutUsBuild.text = String.format(getString(R.string.version_code), PackageInfoCompat.getLongVersionCode(packageInfo))
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
